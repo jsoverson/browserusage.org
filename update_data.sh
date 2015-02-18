596c995b2bf578d5cac0397660fa79b7395117db
Jarrod Overson <joverson@shapesecurity.com>
2015-02-18 13:39:27 -0800

    updated data

--- update_data.sh
+++ update_data.sh
@@ -1,3 +1,9 @@
-BASIC="http://gs.statcounter.com/chart.php?201408=undefined&device=Desktop%20%26%20Tablet&device_hidden=desktop%2Btablet&statType_hidden=browser_version_partially_combined&region_hidden=ww&granularity=monthly&statType=Combine%20Chrome%20(all%20versions)%20%26%20Firefox%20(5%2B)&region=Worldwide&fromInt=201212&toInt=201412&fromMonthYear=2012-12&toMonthYear=2014-12&multi-device=true&csv=1"
+
+FROM=2013-02
+FROMINT=201302
+TO=2015-02
+TOINT=201502
+
+BASIC="http://gs.statcounter.com/chart.php?201408=undefined&device=Desktop%20%26%20Tablet&device_hidden=desktop%2Btablet&statType_hidden=browser_version_partially_combined&region_hidden=ww&granularity=monthly&statType=Combine%20Chrome%20(all%20versions)%20%26%20Firefox%20(5%2B)&region=Worldwide&fromInt=$FROMINT&toInt=$TOINT&fromMonthYear=$FROM&toMonthYear=$TO&multi-device=true&csv=1"
 
 curl $BASIC > data/basic_data.csv
